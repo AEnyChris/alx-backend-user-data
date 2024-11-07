c42ccb700da5beff499d62055deb49501472932f
Enyone Christian Achobe <enyonejoseph@gmail.com>
2024-11-07 23:00:04 +0100

    added task 1: A redacting class formatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,6 +2,7 @@
 """A script to obfuscate certain fields in log message"""
 import re
 from typing import List
+import logging
 
 
 def filter_datum(
@@ -14,3 +15,23 @@ def filter_datum(
           'repl': lambda x: r'\g<f>={}'.format(x)}
     res = re.sub(ps['p'](fields, separator), ps['repl'](redaction), message)
     return res
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """initializes an instance"""
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """formats log records to hide sensitive data"""
+        recmsg = super(RedactingFormatter, self).format(record)
+        res = filter_datum(self.fields, self.REDACTION, recmsg, self.SEPARATOR)
+        return res
