4cd5505bcac63c8a86d1299593495c9934b0b3c1
Enyone Christian Achobe <enyonejoseph@gmail.com>
2024-11-12 02:40:34 +0100

    removed some test prints

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -5,6 +5,7 @@ Route module for the API
 from os import getenv
 from api.v1.views import app_views
 from api.v1.auth.auth import Auth
+from api.v1.auth.basic_auth import BasicAuth
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
@@ -16,6 +17,8 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 if os.getenv('AUTH_TYPE') == 'auth':
     auth = Auth()
+elif os.getenv('AUTH_TYPE') == 'basic_auth':
+    auth = BasicAuth()
 
 
 @app.errorhandler(404)
@@ -49,13 +52,9 @@ def handle_before_request():
             ]
     if auth:
         if auth.require_auth(request.path, ex_paths):
-            print(request.path)
-            print(f"req_auth: {auth.require_auth(request.path, ex_paths)}")
             if auth.authorization_header(request) is None:
-                print(f"auth_header: {auth.authorization_header(request)}")
                 abort(401)
             if auth.current_user(request) is None:
-                print(f"current_user: {auth.current_user(request)}")
                 abort(403)
 
 
