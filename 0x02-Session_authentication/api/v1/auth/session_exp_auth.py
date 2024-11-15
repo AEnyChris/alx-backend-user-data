aaa73eea5dbc255574593fcf9e46034a7bdce841
Enyone Christian Achobe <enyonejoseph@gmail.com>
2024-11-15 16:18:07 +0100

    added session expiration function

---
+++ 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""Creates a class to add an expiration date session"""
+import os
+from api.v1.auth.session_auth import SessionAuth
+from datetime import datetime, timedelta
+
+
+class SessionExpAuth(SessionAuth):
+    """session expiration class"""
+    def __init__(self):
+        """initializtion"""
+        try:
+            self.session_duration = int(os.getenv('SESSION_DURATION'))
+        except Exception as e:
+            print(e)
+            self.session_duration = 0
+
+    def create_session(self, user_id=None):
+        """create a new session"""
+        session_id = super().create_session(user_id)
+        if session_id:
+            session_dictionary = {}
+            session_dictionary['user_id'] = user_id
+            session_dictionary['created_at'] = datetime.now()
+            self.user_id_by_session_id[session_id] = session_dictionary
+        return session_id
+
+    def user_id_for_session_id(self, session_id=None):
+        """returns user id based on session id"""
+        if session_id:
+            session_dict = self.user_id_by_session_id.get(session_id)
+            if session_dict:
+                user = session_dict.get('user_id')
+                c_at = session_dict.get('created_at')
+                delta = timedelta(seconds=self.session_duration)
+                if self.session_duration <= 0:
+                    return user
+                elif not c_at:
+                    return None
+                elif c_at + delta < datetime.now():
+                    return None
+                else:
+                    return user
+        return None
