34e3bb66023310b614990c40259aa0cd629b19d5
Enyone Christian Achobe <enyonejoseph@gmail.com>
2024-11-19 09:31:33 +0100

    added annotation to find_user_by

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -7,6 +7,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
 from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.exc import InvalidRequestError
+from typing import Dict, Any
 
 from user import Base, User
 
@@ -41,7 +42,7 @@ class DB:
         self._session.commit()
         return user
 
-    def find_user_by(self, **kwargs):
+    def find_user_by(self, **kwargs: Dict[str, Any]) -> User:
         """
         returns the first row found in the users table
         as filtered by the input
