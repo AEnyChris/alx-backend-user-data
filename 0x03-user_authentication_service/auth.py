f339bbda9a378f7a3e564707b50299b1d30a6b02
Enyone Christian Achobe <enyonejoseph@gmail.com>
2024-11-19 11:59:32 +0100

    added register uesr to auth module

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 """authentication module"""
 import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
 
 
 def _hash_password(password: str) -> bytes:
@@ -11,3 +14,23 @@ def _hash_password(password: str) -> bytes:
     byte_password = password.encode()
     hashed = bcrypt.hashpw(byte_password, bcrypt.gensalt())
     return hashed
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        """initializer"""
+        self._db = DB()
+ 
+    def register_user(self, email: str, password: str) -> User:
+        """registers a user with the given email and password"""
+        try:
+            user = self._db.find_user_by(email = email)
+            if user:
+                raise ValueError(f'User {email} already exists')
+        except NoResultFound:
+            hashpwd = _hash_password(password)
+            user = self._db.add_user(email=email, hashed_password=hashpwd.decode())
+            return user
