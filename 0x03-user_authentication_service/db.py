748ae634d4cabcab1491cf92b4d0d0e46f39a2e4
Enyone Christian Achobe <enyonejoseph@gmail.com>
2024-11-19 10:42:03 +0100

    correction on find_user_by

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """DB module
 """
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, tuple_
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
@@ -47,14 +47,19 @@ class DB:
         returns the first row found in the users table
         as filtered by the input
         """
-        if kwargs:
-            for key in kwargs.keys():
-                if not hasattr(User, key):
-                    raise InvalidRequestError
-            user = self._session.query(User).filter_by(**kwargs).first()
-            if not user:
-                raise NoResultFound
-            return user
+        fields, values = [], []
+        for key, value in kwargs.items():
+            if hasattr(User, key):
+                fields.append(getattr(User, key))
+                values.append(value)
+            else:
+                raise InvalidRequestError()
+        result = self._session.query(User).filter(
+            tuple_(*fields).in_([tuple(values)])
+        ).first()
+        if result is None:
+            raise NoResultFound()
+        return result
 
     def update_user(self, user_id: int, **kwargs: Dict[str, Any]) -> None:
         """updates the user with user_id with attributes in kwargs"""
