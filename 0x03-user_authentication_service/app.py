6f7a248ad7f4902e668372848d6a407827467b33
Enyone Christian Achobe <enyonejoseph@gmail.com>
2024-11-25 00:49:44 +0100

    print statemet removed

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -47,7 +47,7 @@ def logout():
     session_id = request.cookies.get('session_id')
     # print(f'session_id: {session_id}')
     if session_id:
-        print('I got a session_id')
+        # print('I got a session_id')
         user = AUTH.get_user_from_session_id(session_id)
         if user:
             AUTH.destroy_session(user.id)
